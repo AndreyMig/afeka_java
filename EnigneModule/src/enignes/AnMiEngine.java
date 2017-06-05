ce1267fefb63fe05472c5e18f3afce25fd7be8db
Andery <migalovich@gmail.com>
2017-06-05 19:23:50 +0300

    added new engine .....

--- EnigneModule/src/enignes/AnMiEngine.java
+++ EnigneModule/src/enignes/AnMiEngine.java
@@ -5,8 +5,22 @@ public class AnMiEngine {
 	private int speed = 0;
 	
 	
+	public void incSpeed(){
+		
+		speed = speed*2;
+		
+	}
+	
+	public void decSpeed(){
+		
+		speed = speed/2;
+		
+	}
 	
 	
-	
+	@Override
+	public String toString() {
+		return "current speed: " + speed;
+	}
 	
 }
